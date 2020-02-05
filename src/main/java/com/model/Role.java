3554fc62586ea90036a84bcab396459964ee4c74
MeanBlackCat <emma.baltatzis@gmail.com>
2020-02-05 14:12:10 +0100

    added Role.java that will help determine what login site the user will have

---
+++ src/main/java/com/model/Role.java
@@ -0,0 +1,23 @@
+package com.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "role")
+public class Role {
+
+    @Column(name = "role_id")
+    private Integer id;
+
+    @Column(name = "name")
+    private String roleName;
+
+    public Integer getId() { return id; }
+
+    public void setId(Integer id) { this.id = id; }
+
+    public String getRoleName() { return roleName; }
+
+    public void setRoleName(String roleName) { this.roleName = roleName}
+
+}
