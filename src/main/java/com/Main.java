09bc3adc90bac6f70b1a19e01a26f3cb60418cf6
Nandeyanen123 <emickols@kth.se>
2020-02-05 11:21:25 +0100

    Made some changes back to previous structure for encapsulation and changed Views to better represent the project (and for added functionality)

--- src/main/java/com/Main.java
+++ src/main/java/com/Main.java
@@ -1,38 +1,14 @@
 package com;
 
-import com.model.Person;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
+
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-import javax.sql.DataSource;
-
-
-//@SpringBootApplication()
-@EnableAutoConfiguration
-@ComponentScan
-@Configuration
-@EnableJpaRepositories
-
+@SpringBootApplication
 public class Main {
 
-    /*@Value("${spring.datasource.url}")
-    private String dbUrl;
-
-    @Autowired
-    private DataSource dataSource;
-*/
     public static void main(String[] args) throws Exception {
         SpringApplication.run(com.Main.class, args);
     }
-    @Bean
-    public Person getPeron(){return new Person();}
 
 }
--- src/main/java/com/controller/MainController.java
+++ src/main/java/com/controller/MainController.java
@@ -2,40 +2,28 @@ package com.controller;
 
 import com.model.Person;
 import com.service.PersonRepository;
-import com.zaxxer.hikari.HikariConfig;
-import com.zaxxer.hikari.HikariDataSource;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.lang.Nullable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-import javax.sql.DataSource;
-import java.sql.Connection;
-import java.sql.ResultSet;
+import javax.validation.ConstraintViolationException;
 import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.ArrayList;
-import java.util.Map;
+import java.sql.SQLIntegrityConstraintViolationException;
+
 
 @Controller
-//@SpringBootApplication
 @RequestMapping(path="/")
 public class MainController {
 
-  @Autowired
-  private Person p;
   @Autowired
   private PersonRepository personRepository;
 
   @PostMapping(path="/report1test/add")
   public String addNewPerson (@Nullable @RequestParam String name, String surName, String ssn,
                               String email, String password, Integer roleId, String userName){
-    //Person p = new Person();
+    Person p = new Person();
     p.setName(name);
     p.setSurName(surName);
     p.setSsn(ssn);
@@ -43,21 +31,21 @@ public class MainController {
     p.setPassword(password);
     p.setRoleId(roleId);
     p.setUserName(userName);
-    personRepository.save(p);
+    try {
+        personRepository.save(p);
+    } catch(Exception ex){
+        return "redirect:/error";
+    }
     return "redirect:/report1testresult";
   }
 
   @GetMapping(path="/report1testresult")
-  //public @ResponseBody String getAllPeople(Model model){
-      public String getAllPeople(Model model){
+  public String getAllPeople(Model model){
       Iterable<Person> people = personRepository.findAll();
       model.addAttribute("people",people);
     return "report1testresult";
   }
 
-  /*public static void main(String[] args) throws Exception {
-    SpringApplication.run(MainController.class, args);
-  }*/
 
     @GetMapping("/report1test")
     public String personForm(Model model) {
@@ -79,38 +67,5 @@ public class MainController {
   String repTest() {
     return "report1test";
   }
-
-
-  /*@RequestMapping("/db")
-  String db(Map<String, Object> model) {
-    try (Connection connection = dataSource.getConnection()) {
-      Statement stmt = connection.createStatement();
-      stmt.executeUpdate("CREATE TABLE IF NOT EXISTS ticks (tick timestamp)");
-      stmt.executeUpdate("INSERT INTO ticks VALUES (now())");
-      ResultSet rs = stmt.executeQuery("SELECT tick FROM ticks");
-
-      ArrayList<String> output = new ArrayList<String>();
-      while (rs.next()) {
-        output.add("Read from DB: " + rs.getTimestamp("tick"));
-      }
-
-      model.put("records", output);
-      return "db";
-    } catch (Exception e) {
-      model.put("message", e.getMessage());
-      return "error";
-    }
-  }
-
-  @Bean
-  public DataSource dataSource() throws SQLException {
-    if (dbUrl == null || dbUrl.isEmpty()) {
-      return new HikariDataSource();
-    } else {
-      HikariConfig config = new HikariConfig();
-      config.setJdbcUrl(dbUrl);
-      return new HikariDataSource(config);
-    }
-  }*/
 
 }
