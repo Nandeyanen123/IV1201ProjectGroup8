396e0dab3e258fd33bfe62a6c751c10f97d7d42e
Marva80 <mmarzban80@gmail.com>
2020-02-19 22:38:56 +0100

    Spring starter security is added again

--- src/main/java/com/config/AppSecurityConfig.java
+++ src/main/java/com/config/AppSecurityConfig.java
@@ -10,11 +10,9 @@ import org.springframework.security.authentication.dao.DaoAuthenticationProvider
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
-import org.springframework.security.web.util.matcher.AndRequestMatcher;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
@@ -29,24 +27,28 @@ public class AppSecurityConfig extends WebSecurityConfigurerAdapter {
     {
         DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
         provider.setUserDetailsService(userDetailsService);
-        provider.setPasswordEncoder(new BCryptPasswordEncoder());
+        provider.setPasswordEncoder(NoOpPasswordEncoder.getInstance());
         return provider;
     }
 
-    @Override
+   /* @Override
     protected void configure(HttpSecurity http) throws Exception {
-         http.csrf()
-                 .disable()
-                 .authorizeRequests()
-                 .antMatchers("/", "/register/**", "/error")
-                    .permitAll()
-                 .anyRequest().authenticated()
-                 .and()
-                 .formLogin()
-                 .loginPage("/login").permitAll()
-                .and().logout().invalidateHttpSession(true)
-                .clearAuthentication(true)
-                .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
-                .logoutSuccessUrl("/logout-success").permitAll();
-    }
+        http
+                .authorizeRequests()
+                .antMatchers("/","/register/**","/error").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/login").defaultSuccessUrl("/profile")
+                .failureUrl("/login?error=true")
+                .permitAll()
+                .and()
+                .logout()
+                .invalidateHttpSession(true)
+                .permitAll().and()
+                .csrf()
+                .disable();
+
+
+    }*/
 }
