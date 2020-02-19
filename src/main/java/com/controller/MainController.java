dec64c67afa6cee0ad4426738274812bac2b8970
Daniel <danielwesterlund17@gmail.com>
2020-02-19 11:43:27 +0100

    Moved navigation bar to its own header file. Created register.html with register form. Added support for /register -> register.html in MainController

--- src/main/java/com/controller/MainController.java
+++ src/main/java/com/controller/MainController.java
@@ -8,6 +8,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.constraints.NotNull;
+
 
 @Controller
 @RequestMapping(path="/")
@@ -16,23 +18,23 @@ public class MainController {
   @Autowired
   private PersonRepository personRepository;
 
-  @PostMapping(path="/report1test/add")
-  public String addNewPerson (@Nullable @RequestParam String name, String surName, String ssn,
-                              String email, String password, Integer roleId, String userName){
+  @PostMapping(path="/register/add")
+  public String addNewPerson (@NotNull @RequestParam String name, String surName, String ssn,
+                              String email, String password, String userName){
     Person p = new Person();
     p.setName(name);
     p.setSurName(surName);
     p.setSsn(ssn);
     p.setEmail(email);
     p.setPassword(password);
-    p.setRoleId(roleId);
+    p.setRoleId(2);
     p.setUserName(userName);
     try {
         personRepository.save(p);
     } catch(Exception ex){
         return "redirect:/error";
     }
-    return "redirect:/report1testresult";
+    return "redirect:/login";
   }
 
   @GetMapping(path="/report1testresult")
@@ -43,15 +45,15 @@ public class MainController {
   }
 
 
-    @GetMapping("/report1test")
+    @GetMapping("/register")
     public String personForm(Model model) {
         model.addAttribute("person", new Person());
-        return "report1test";
+        return "register";
     }
 
-    @PostMapping("/report1test")
+    @PostMapping("/register")
     public String personSubmit(@ModelAttribute Person person) {
-        return "report1testresult";
+        return "login";
     }
 
   @RequestMapping("/")
