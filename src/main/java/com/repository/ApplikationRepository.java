882935c29fc1d6770e243f34a77352c5343fbba5
Daniel <danielwesterlund17@gmail.com>
2020-03-04 01:26:31 +0100

    Added Repository for Applikation, Availability and Status classes

---
+++ src/main/java/com/repository/ApplikationRepository.java
@@ -0,0 +1,19 @@
+package com.repository;
+
+import com.model.Applikation;
+import com.model.Competence;
+import com.model.Competence_Profile;
+import com.model.Person;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ * This is the repository class for Competence profile.
+ * It extends JpaRepository
+ */
+@Repository
+public interface ApplikationRepository  extends JpaRepository<Applikation, Integer> {
+    Applikation findByPerson(Person person);
+}
+
+
