76fb460d3828c27fae1f0bf0e5c621b915adef5c
Daniel <danielwesterlund17@gmail.com>
2020-03-06 12:59:28 +0100

    Removed insertable from Person, for be able to add new Availability to DB. Added @DateTimeFormat(pattern = "yyyy-MM-dd") for Date objects. Added Constructors. Added methods that removes timestamp from Date objects, so only yyyy-MM-dd is left

--- src/main/java/com/model/Availability.java
+++ src/main/java/com/model/Availability.java
@@ -1,6 +1,9 @@
 package com.model;
 
+import org.springframework.format.annotation.DateTimeFormat;
+
 import javax.persistence.*;
+import javax.validation.constraints.NotNull;
 import java.util.Date;
 
 /**
@@ -16,14 +19,23 @@ public class Availability {
     private Integer availabilityId;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "person_id", insertable = false, updatable = false)
+    @JoinColumn(name = "person_id", updatable = false)
     private Person person;
 
     @Column(name = "from_date")
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
     private Date fromDate;
 
     @Column(name = "to_date")
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
     private Date toDate;
+
+    public Availability(){}
+    public Availability(Person person, Date from_date, Date to_date){
+        this.person = person;
+        this.fromDate = from_date;
+        this.toDate = to_date;
+    }
 
     public Integer getAvailabilityId() {
         return availabilityId;
@@ -55,5 +67,14 @@ public class Availability {
 
     public void setToDate(Date toDate) {
         this.toDate = toDate;
+    }
+
+    public String getFromDateFixed(){
+        return fromDate.toString().substring(0, 10);
+    }
+
+
+    public String getToDateFixed(){
+        return toDate.toString().substring(0, 10);
     }
 }
