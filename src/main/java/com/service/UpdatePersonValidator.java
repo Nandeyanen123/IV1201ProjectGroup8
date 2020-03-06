c656bcf0a629a84ce726dbc0024a2b76ae101a48
Daniel <danielwesterlund17@gmail.com>
2020-03-06 16:51:59 +0100

    Removed unnecessary personRepository object

--- src/main/java/com/service/UpdatePersonValidator.java
+++ src/main/java/com/service/UpdatePersonValidator.java
@@ -16,9 +16,6 @@ import org.springframework.validation.Validator;
 @Component
 public class UpdatePersonValidator implements Validator {
 
-    @Autowired
-    PersonRepository personRepository;
-
     /**
      * This method is used to see if the Person class equals the class parameter.
      * @param clazz This is the only parameter of the method supports
